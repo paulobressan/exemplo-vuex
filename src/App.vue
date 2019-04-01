a87652a877040ded239f0995bcb1d3940c63937c
paulobressan <paulo.bressan@outlook.com>
2019-03-31 22:47:03 -0300

    Adicionando vuex

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{count}}</h1>
+    <button @click="incrementa">+</button>
+    <button @click="decrementa">-</button>
+    <br>
+    <button @click="incrementaColor" :class="color">Local</button>
+    <br>
+    <ul>
+      <li v-for="todo of doneTodos" :key="todo.id">{{todo.text}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapGetters } from "vuex";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      localCount: 4
+    };
+  },
+  methods: {
+    incrementa() {
+      this.$store.commit("increment");
+    },
+    decrementa() {
+      this.$store.commit("decrement");
+    },
+    incrementaColor() {
+      this.localCount++;
+      //Exemplo de alterando um item da lsita de todo depois de 1 minuto.
+      //Alteração feita atraves de commit
+      setTimeout(() => {
+        this.$store.commit("doneTodo");
+      }, 1000);
+    }
+  },
+  // Para usar o estado do vuex, temos que chamar a variavel passada por props, this.$store.state
+  // Ou usar o mapState para maperar os estados para o componente
+  //computed: mapState(["count"])
+  // Porem podemos ter mais de um dado computado e para isso podemos usar o spread operator(...) para desmenbrar
+  // O retorno da função mapState e computar um objeto computado
+  computed: {
+    color() {
+      return this.localCount > 10 ? "red" : "blue";
+    },
+    ...mapState({
+      // Para mapear, temos usar um callback esperando o state e o retorno no caso a baixo é o count que esta definido no state da store
+      count: state => state.count,
+      // Podemos realizar operações com o estado da store e o estado local do componente
+      countPlusLocalCount(state) {
+        return state.count + this.localCount;
+      }
+    }),
+    //Acessando um getter
+    todoDone() {
+      return this.$store.getters.doneTodos;
+    },
+    //Acessando um getter com um helper mapGetters
+    ...mapGetters(["doneTodos"])
   }
-}
+  //Podemos passar um array com os nomes dos estados que quemos utilizar no contexto do componente
+  // Evitando sempre chamar um callback para mapear os state
+  /**
+   ...mapState([
+      'count'
+   ])
+   */
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.red {
+  background: red;
+}
+
+.blue {
+  background: blue;
+}
 </style>
+
+
+// {
+//     count: state => state.count,
+//     countAlias: "count",
+//     countPlusLocalState(state) {
+//       return state.count + this.localCount;
+//     }
+//   }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,50 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+//Usando o vuex
+Vue.use(Vuex)
+
+// Criando um store Global
+const store = new Vuex.Store({
+  // Estado da aplicação
+  state: {
+    count: 3,
+    // definindo uma lista
+    todo: [
+      { id: 1, text: 'teste', done: true },
+      { id: 2, text: 'teste2', done: false }
+    ]
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--,
+    //Exemplo mudando o estado de um item de uma lista
+    //O getter que usa essa lista é tambem atualizado
+    doneTodo: state => {
+      state.todo = state.todo.map((todo, index) => {
+        if (index == 1)
+          todo.done = !todo.done
+        return todo
+      })
+      return state.todo
+    }
+  },
+  /** 
+   * O Vuex nos permite definir getters no store. 
+   * Você pode pensar neles como dados computados para os stores. 
+   * Como os dados computados, o resultado de um getter é armazenado em cache com base em suas dependências
+   *  e só será reavaliado quando algumas de suas dependências forem alteradas.
+  */
+  getters: {
+    // Getter para filtrar os done(concluido) da lista de todo no estado
+    doneTodos: state => state.todo.filter(todo => todo.done)
+  }
+});
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
